99b4f6879d68fefced4c38501a28ce88a7846a28
Aleksandr Alekseev <alexander.alekseev@exness.com>
2024-11-03 18:42:42 +0200

    add setup.sh script

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+# Check if Homebrew is installed
+if test ! $(which brew); then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null ## /dev/null skips pressing enter for the installation
+fi
+
+# Update Homebrew
+echo "Installing/updating Homebrew boundle..."
+brew bundle --file=./homebrew/Brewfile
+
+# Install App Store apps
+echo "Installing App Store apps..."
+mas install 904280696  # Things
+mas install 1461845568 # Gifox
+mas install 310633997  # WhatsApp
+
+# Symlink dotfiles
+echo "Symlinking dotfiles..."
+stow -t ~ nvim
+stow -t ~ scripts
+stow -t ~ tmux
+stow -t ~ wezterm
+stow -t ~ zsh
+stow -t ~/Library/Application\ Support/k9s k9s
+
