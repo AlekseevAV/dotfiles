e3c75806c0839a5b202c56b680bd470b60c9a8db
Aleksandr Alekseev <alexander.alekseev@exness.com>
2025-01-23 09:48:51 +0200

    Add new mini plugins

--- nvim/.config/nvim/lua/plugins/mini_nvim.lua
+++ nvim/.config/nvim/lua/plugins/mini_nvim.lua
@@ -15,6 +15,21 @@ return {
     require("mini.notify").setup()
     require("mini.tabline").setup()
     require("mini.trailspace").setup()
+    require("mini.move").setup({
+      mappings = {
+        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+        left = "<C-M-h>",
+        right = "<C-M-l>",
+        down = "<C-M-j>",
+        up = "<C-M-k>",
+
+        -- Move current line in Normal mode
+        line_left = "<C-M-h>",
+        line_right = "<C-M-l>",
+        line_down = "<C-M-j>",
+        line_up = "<C-M-k>",
+      },
+    })
     require("mini.indentscope").setup({ draw = {
       animation = function()
         return 0
